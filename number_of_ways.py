5a35f343368e56514696ff449d1e388f943336ba
marco.scotto <marco.scotto@gmail.com>
2023-03-15 14:42:55 +0100

    Fix initialization of paths
    Paths should have been a list of one list element, rather than a list of one int element

--- number_of_ways.py
+++ number_of_ways.py
@@ -21,7 +21,7 @@ def numberOfWays(startPos: int, endPos: int, k: int) -> int:
     # loop k times
     for i in tqdm(range(k)):
         for path in paths:
-            new_path = path.copy()
+            new_path = paths.copy()
             last_position = new_path[-1]
 
             # exist fast if not going to make to end
